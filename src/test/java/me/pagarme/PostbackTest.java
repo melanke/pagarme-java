b3ca4776a1c144204bd3fe768ab73cece46425b4
Leonardo Barbosa <leonardo.barbosa@pagar.me>
2017-12-13 14:41:30 -0200

    add validate request test

--- src/test/java/me/pagarme/PostbackTest.java
+++ src/test/java/me/pagarme/PostbackTest.java
@@ -1,20 +1,39 @@
 package me.pagarme;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import java.io.FileReader;
+import java.util.Scanner;
+
 import me.pagar.model.PagarMe;
+import me.pagar.model.Postback;
+import me.pagar.util.JSONUtils;
+
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.Before;
 
 public class PostbackTest extends BaseTest {
-    
+
     @Before
     public void setUp() {
-        super.setUp();
+        PagarMe.init("ak_test_21eI4THNDJx7vkJRuAmXYlFACIaxnS");
     }
-    
+
     @Test
     public void testNullOrEmptyParams() {
         Assert.assertFalse( PagarMe.validateRequestSignature("", "") );
         Assert.assertFalse( PagarMe.validateRequestSignature(null, null) );        
+    }
+
+    @Test
+    public void testPostbackValidation() throws Throwable {
+        Scanner postbackFile = new Scanner(new FileReader("src/test/resources/__files/1/postback.json"));
+        JsonParser parser = new JsonParser();
+        JsonObject jo = parser.parse(postbackFile.useDelimiter("\\Z").next()).getAsJsonObject();
+        Postback postback = JSONUtils.getAsObject(jo, Postback.class);
+        postbackFile.close();
+
+        Assert.assertTrue(PagarMe.validateRequestSignature(postback.getPayload(), postback.getSignature()));
     }
 }
